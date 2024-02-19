492583e4c3f42dd6322b76914d492431bf3c5e2f
Mark Kozhydlo <99179725+kozhydlo@users.noreply.github.com>
2024-02-19 23:25:58 +0200

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,17 @@
+import React from 'react';
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
+
 import projImg1 from "../assets/img/proj1.png";
 import projImg2 from "../assets/img/proj2.png";
 import projImg3 from "../assets/img/proj3.png";
 import projImg4 from "../assets/img/proj4.png";
 import projImg5 from "../assets/img/proj5.png";
 import projImg6 from "../assets/img/proj7.png";
+import projImg7 from "../assets/img/proj8.png";
+import projImg8 from "../assets/img/proj9.png";
+import projImg9 from "../assets/img/proj10.png"; 
+
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -45,6 +51,24 @@ export const Projects = () => {
     },
   ];
 
+  const projects2 = [ // Доданий новий об'єкт projects2
+    {
+      title: "Gta 6 Form",
+      description: "Development",
+      imgUrl: projImg7,
+    },
+    {
+      title: "Adventure",
+      description: "Design & Development",
+      imgUrl: projImg8,
+    },
+    {
+      title: "Task Manager",
+      description: "Design ",
+      imgUrl: projImg9,
+    },
+  ];
+
   return (
     <section className="project" id="projects">
       <Container>
@@ -70,20 +94,17 @@ export const Projects = () => {
                   <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
                     <Tab.Pane eventKey="first">
                       <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
+                        {projects.map((project, index) => (
+                          <ProjectCard key={index} {...project} />
+                        ))}
                       </Row>
                     </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>It's still empty here</p>
+                    <Tab.Pane eventKey="second">
+                      <Row>
+                        {projects2.map((project, index) => (
+                          <ProjectCard key={index} {...project} />
+                        ))}
+                      </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
                       <p>It's still empty here</p>
@@ -95,7 +116,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} alt="Background"></img>
     </section>
   )
 }
