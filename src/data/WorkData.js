13a3ebc3bd638297e4e8feb2bfe761bcf8df66a5
Mark Kozhydlo <99179725+kozhydlo@users.noreply.github.com>
2024-08-06 20:40:45 +0300

    Add files via upload

---
+++ src/data/WorkData.js
@@ -0,0 +1,54 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"PassKey",
+        description:"is a password generator application with a very cool interface, beautiful design and also with interesting new features that are not found in other similar applications.",
+        tags:["react native","figma","Mobile development"],
+        
+        demo:"https://kozhydlo-passkey.netlify.app/",
+        github:"https://github.com/kozhydlo/PassKey"
+    },
+    {
+        id:2,
+        name:"Crypto App",
+        description:"this is a crypto portfolio project where you can track your crypto currency in a beautiful interface",
+        tags:["react","sass", "nodejs","api"],
+        
+        demo:"#",
+        github:"https://github.com/kozhydlo/crypto-app"
+        
+    },
+    {
+        id:3,
+        name:"Link Shortening",
+        description:"this is a website where you can shorten links and get statistics on clicks on this link",
+        tags:["react","nodejs","MongoDB", "express"],
+        
+        demo:"#",
+        github:"https://github.com/kozhydlo/link-shortening"
+    },
+    {
+        id:4,
+        name:"ECO-BLOG",
+        description:"is a social network where you can share your environmental thoughts with others and discuss it and also post",
+        tags:["react","css","react-router", "MongoDB", "express", "Redux", "nodejs"],
+        
+        demo:"#",
+        github:"https://github.com/kozhydlo/ECO-BLOG"
+       
+    },{
+        id:5,
+        name:"Movie Parser",
+        description:"this is a parser that will parse all the movies from the uakino site for you",
+        tags:["Python"],
+        
+        demo:"#",
+        github:"https://github.com/kozhydlo/movie-parser"
+        
+    },
+    
+    
+  
+] 
